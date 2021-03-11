87cf148db870f31f02bb655b27e76121575629ea
Alexandre de Souza <alexandre@aledsz.com.br>
2021-03-11 00:54:30 -0300

    :sparkles: add function to handle configurations

--- lib/ecto_xsd.ex
+++ lib/ecto_xsd.ex
@@ -7,4 +7,15 @@ defmodule EctoXSD do
   and provides a way to validate the element with his
   equivalent XSD.
   """
+
+  @typep system_env() :: {:system, binary()}
+  @typep configuration :: [] | keyword(binary() | system_env())
+
+  @doc """
+  Gets the XSD Paths configuration
+  """
+  @spec get_config() :: configuration()
+  def get_config do
+    Application.get_env(:ecto_xsd, :xsd_paths, [])
+  end
 end
