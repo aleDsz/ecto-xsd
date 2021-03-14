512936c0a9b91a0919fc758e5790e5b7605d075b
Alexandre de Souza <alexandre@aledsz.com.br>
2021-03-14 15:48:53 -0300

    :art: improve macro validations

--- lib/ecto_xsd/schema.ex
+++ lib/ecto_xsd/schema.ex
@@ -11,58 +11,64 @@ defmodule EctoXSD.Schema do
   end
   ``` 
   """
-  import EctoXSD
 
   @doc false
   defmacro __using__(_opts) do
     quote do
       use Ecto.Schema
+      import EctoXSD.Schema, only: [xsd: 1]
 
-      @before_compile Ecto.XSD
+      @before_compile EctoXSD.Schema
       @primary_key false
-
-      Module.put_attribute(__MODULE__, :ecto_xsd_path, nil)
     end
   end
 
   @doc false
-  defmacro __before_compile__(_, _) do
+  defmacro __before_compile__(_env) do
     quote do
-      require EctoXSD.Schema
-
       def __xsd__(:path), do: @ecto_xsd_path
     end
   end
 
   @doc false
   defmacro xsd(xsd_path) do
-    with {:ok, path} <- __xsd__(xsd_path) do
-      Module.put_attribute(__MODULE__, :ecto_xsd_path, path)
+    __xsd__(xsd_path)
+  end
+
+  defp __xsd__(xsd_path) do
+    postlude =
+      quote do
+        Module.register_attribute(__MODULE__, :ecto_xsd_path, accumulate: false)
+
+        xsd_path =
+          case unquote(xsd_path) do
+            xsd_path when is_binary(xsd_path) ->
+              xsd_path
+
+            xsd_config when is_atom(xsd_config) ->
+              EctoXSD.get_config(xsd_config)
+          end
+
+        path =
+          with {:file, true} <- {:file, File.exists?(xsd_path)},
+               {:extension, true} <- {:extension, String.ends_with?(xsd_path, ".xsd")} do
+            xsd_path
+          else
+            {:file, false} ->
+              raise File.Error, path: xsd_path, action: "exists?", reason: "doesn't exists"
+
+            {:extension, false} ->
+              raise File.Error,
+                path: xsd_path,
+                action: "correct extension?",
+                reason: "doesn't have .xsd extension"
+          end
+
+        Module.put_attribute(__MODULE__, :ecto_xsd_path, path)
+      end
+
+    quote do
+      unquote(postlude)
     end
-  end
-
-  defp __xsd__(xsd_config) when is_atom(xsd_config) do
-    get_config(xsd_config)
-    |> validate_xsd_path()
-  end
-
-  defp __xsd__(xsd_path) when is_binary(xsd_path) do
-    validate_xsd_path(xsd_path)
-  end
-
-  defp validate_xsd_path(xsd_path) when is_binary(xsd_path) do
-    with true <- File.exists?(xsd_path),
-         true <- String.ends_with?(xsd_path, ".xsd") do
-      xsd_path
-    else
-      _ ->
-        raise raise_path()
-    end
-  end
-
-  defp raise_path do
-    """
-    Raise this
-    """
   end
 end
